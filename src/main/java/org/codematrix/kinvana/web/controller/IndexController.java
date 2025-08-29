1e29d7ea1f3887715bfaa60e145afdfce493c0a0
Informatica <informatica@alumno.local>
2025-08-29 14:01:02 -0600

    Botones terminados

--- src/main/java/org/codematrix/kinvana/web/controller/IndexController.java
+++ src/main/java/org/codematrix/kinvana/web/controller/IndexController.java
@@ -1,10 +1,13 @@
 package org.codematrix.kinvana.web.controller;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
 import org.codematrix.kinvana.dominio.service.IClienteService;
 import org.codematrix.kinvana.persistence.entity.Cliente;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +38,46 @@ public class IndexController {
 
     public void agregarCliente(){
         this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente(){
+        logger.info("Cliente a guardar: " + this.clienteSeleccionado);
+        //Agregar (insertar)
+        if (this.clienteSeleccionado.getCodigoCliente() == null){
+            this.clienteService.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage("Cliente agregado"));
+        }
+        //modificar (update)
+        else{
+            this.clienteService.guardarCliente(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente actualizado"));
+        }
+        //ocultar la venta modal
+        PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+        //Actualizar la tabla con un metodo AJAX
+        PrimeFaces.current().ajax().update("formulario-clientes:mensaje_emergente",
+                                            "formulario-clientes:tabla-clientes");
+        //Reset de clienteSeleccionado
+        this.clienteSeleccionado = null;
+    }
+
+    public void eliminarCliente(){
+        //mostrar en la consola
+        logger.info("Cliente a eliminar: " + this.clienteSeleccionado);
+        //llamar a nuestro servicio de eliminacion de Cliente
+        this.clienteService.EliminarCliente(clienteSeleccionado);
+        //Eliminarlo de la lista de clientes
+        this.clientes.remove(clienteSeleccionado);
+        //Limpiar nuestro cliente Seleccionado
+        this.clienteSeleccionado = null;
+        //enviar un mensaje emergente de confirmacion
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente Eliminado Correctamente"));
+        //Actualiar la tabla con AJAX
+        PrimeFaces.current().ajax().update(
+                "formulario-clientes:mensaje_emergente",
+                "formulario-clientes:tabla-clientes");
     }
 
 }
